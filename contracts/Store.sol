6e9398660e8451bd079da703b7a3222b805fdb71
deanamiel <deanamiel1@gmail.com>
2022-03-31 16:54:11 -0400

    Added update, remove listing and buy NFT functionality

--- contracts/Store.sol
+++ contracts/Store.sol
@@ -57,4 +57,73 @@ contract Store is ERC721Holder {
 
         emit NewListing(msg.sender, _price);
     }
+
+    function updateListing(
+        address _collection,
+        uint256 _tokenID,
+        uint256 _newPrice
+    ) external {
+        require(
+            IERC721(_collection).supportsInterface(0x80ac58cd),
+            "Collection address is not valid ERC721 address"
+        );
+        require(
+            msg.sender == registry[_collection][_tokenID].owner,
+            "Only owner of listing can update listing"
+        );
+
+        registry[_collection][_tokenID].price = _newPrice;
+
+        emit UpdateListing(msg.sender, _newPrice, _collection, _tokenID);
+    }
+
+    function removeListing(address _collection, uint256 _tokenID) external {
+        require(
+            IERC721(_collection).supportsInterface(0x80ac58cd),
+            "Collection address is not valid ERC721 address"
+        );
+        require(
+            msg.sender == registry[_collection][_tokenID].owner,
+            "Only owner of listing can remove listing"
+        );
+
+        delete (registry[_collection][_tokenID]);
+        totalSupply--;
+        IERC721(_collection).safeTransferFrom(
+            address(this),
+            msg.sender,
+            _tokenID
+        );
+
+        emit RemoveListing(msg.sender);
+    }
+
+    function buyNFT(address _collection, uint256 _tokenID) external payable {
+        require(
+            IERC721(_collection).supportsInterface(0x80ac58cd),
+            "Collection address is not valid ERC721 address"
+        );
+        require(
+            registry[_collection][_tokenID].owner != address(0),
+            "NFT listing does not exist"
+        );
+        require(
+            msg.value >= registry[_collection][_tokenID].price,
+            "Inadequate funds to buy NFT"
+        );
+
+        address payable owner = registry[_collection][_tokenID].owner;
+
+        delete (registry[_collection][_tokenID]);
+        totalSupply--;
+
+        IERC721(_collection).safeTransferFrom(
+            address(this),
+            msg.sender,
+            _tokenID
+        );
+        owner.transfer(msg.value);
+
+        emit BuyNFT(msg.sender, owner, msg.value);
+    }
 }
